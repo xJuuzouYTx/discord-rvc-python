723d056e16020ae0a7f5a67f5853ef0b93f06da1
JuuzouYT <xdcuentta65@gmail.com>
2023-09-16 23:59:24 -0500

    [ADD] apt install pip

--- autoinstall.sh
+++ autoinstall.sh
@@ -2,4 +2,5 @@ sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.9
+sudo apt install python3-pip
 pip install -r requirements.txt
